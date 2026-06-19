84febb5a1754268f2cc3e2ef0a90f99b1e3be7ea
Mohit Darji <mohitdarji0213@gmail.com>
2026-06-19 09:26:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const app = express()
 
 // Middleware
-app.use(cors({ origin: process.env.FRONTEND_URL || 'https://bhartiya-hospital.vercel.app', credentials: true }))
+app.use(cors({ origin: process.env.FRONTEND_URL || 'https://hf-bice.vercel.app', credentials: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
